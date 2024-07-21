9bc61358b4599c209778cd181d28e96945b88e68
kojhack <122357318+kojhack@users.noreply.github.com>
2024-07-21 23:08:11 +0000

    Update readme.js

--- readme.js
+++ readme.js
@@ -1,6 +1,5 @@
 module.exports = `### Hi there 👋
 
-
 - 🌱 I’m currently learning Java/Python
 - 👯 I’m looking to collaborate on Java app projects
 - 🤔 I’m looking for help with development of projects
@@ -9,36 +8,23 @@ module.exports = `### Hi there 👋
 - 😄 Pronouns: He/Him
 - ⚡ Fun fact: Technology💻, Manga, Basketball🏀
 
-
 <h3 align="left">Connect with me:</h3>
 <p align="left">
 <a href="https://www.linkedin.com/in/ohini-j%C3%A9r%C3%A9mie-kuakuvi" target="blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="https://www.linkedin.com/in/ir%c3%a8ne-amedji-%f0%9f%87%b9%f0%9f%87%ac-%e2%ad%90%ef%b8%8f-388861243/" height="30" width="40" /></a>
 </p>
 
-
- 
-          
-
-
 ### My daily routine :
-```mermaid
+\`\`\`mermaid
    graph TD;
         Code-->Eat;
         Train-->Eat;
         Eat-->Sleep;
         Sleep-->Code;
-```
-<!--<p><img align="left" src="https://github-readme-stats.vercel.app/api/top-langs?username=kojhack&show_icons=true&locale=en&layout=compact" alt="kojhack" /></p>
+\`\`\`
 
- <p><img align="center" src="https://github-readme-streak-stats.herokuapp.com/?user=kojhack&" alt="kojhack" /></p>-->
- 
 ![GHstats](https://github-readme-stats.vercel.app/api?username=kojhack&show_icons=true)
-        
          
 ![Profile views counter](https://komarev.com/ghpvc/?username=rishavanand&&style=flat-square)  
-
-
-
 
 Added the \`Fri 03 May 2024\`
 
